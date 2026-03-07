2511933586d605dbe926f1fdfd8d0ab62e521f3a
nast791 <nast791@gmail.com>
2026-03-07 13:37:49 +0300

    start

---
+++ nuxt.config.ts
@@ -0,0 +1,72 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import svgLoader from 'vite-svg-loader';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2026-03-07',
+  devtools: { enabled: true },
+  features: {
+    devLogs: false,
+  },
+  vite: {
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: 'window',
+        },
+      },
+    },
+    plugins: [svgLoader({ svgo: false }), tailwindcss()],
+    css: {
+      transformer: 'lightningcss',
+    },
+    build: {
+      cssMinify: 'lightningcss',
+    },
+  },
+  nitro: {
+    compressPublicAssets: true,
+  },
+  app: {
+    head: {
+      titleTemplate: 'UnPlugged: %s',
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content:
+            'viewport-fit=cover, width=device-width, initial-scale=1, user-scalable=1, minimum-scale=1, maximum-scale=5',
+        },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'theme-color', content: '#fff' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'alternate icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/icon-180.png' },
+        { rel: 'manifest', href: '/manifest.json' },
+      ],
+    },
+    pageTransition: false,
+    layoutTransition: false,
+  },
+  css: ['~/assets/styles.css'],
+  modules: ['@pinia/nuxt', '@nuxt/image'],
+  // image: {
+  //   format: ['avif', 'webp'], // AVIF приоритетнее в 2026 году
+  //   screens: {
+  //     sm: 360,
+  //     md: 767,
+  //     lg: 1025,
+  //     xl: 1920,
+  //   },
+  // },
+  typescript: {
+    strict: true,
+  },
+  spaLoadingTemplate: false,
+  experimental: {
+    payloadExtraction: true,
+    typedPages: true,
+  },
+})
